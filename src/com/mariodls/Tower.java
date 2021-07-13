d3e72d38e0f320b594f324a61d842ad42260445e
Marios Diles <marios.diles@gmail.com>
2021-07-13 19:17:17 +0300

    Added Class Tower, with private fields, constructor and getters

---
+++ src/com/mariodls/Tower.java
@@ -0,0 +1,32 @@
+package com.mariodls;
+
+public class Tower {
+
+    private String model;
+    private String manufacturer;
+    private String powerSupply;
+    private Dimensions dimensions;
+
+    public Tower(String model, String manufacturer, String powerSupply, Dimensions dimensions) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.powerSupply = powerSupply;
+        this.dimensions = dimensions;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getPowerSupply() {
+        return powerSupply;
+    }
+
+    public Dimensions getDimensions() {
+        return dimensions;
+    }
+}
