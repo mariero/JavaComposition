a58d3f006a951c7080d9f04fa4b0a8521d0215c0
Marios Diles <marios.diles@gmail.com>
2021-07-13 19:22:18 +0300

    Added Class PersonalComputer, with private fields, constructor and getters

--- src/com/mariodls/PersonalComputer.java
+++ src/com/mariodls/PersonalComputer.java
@@ -2,4 +2,25 @@ package com.mariodls;
 
 public class PersonalComputer {
 
+    private Tower tower;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+    public PersonalComputer(Tower tower, Monitor monitor, Motherboard motherboard) {
+        this.tower = tower;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public Tower getTower() {
+        return tower;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public Motherboard getMotherboard() {
+        return motherboard;
+    }
 }
